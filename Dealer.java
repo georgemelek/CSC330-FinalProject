cf55a42bdd4b632b02c98b7a3ba4e175b28b4a46
georgemelek <55765541+georgemelek@users.noreply.github.com>
2021-04-22 16:11:54 -0400

    Added pseudo-code for dealer

---
+++ Dealer.java
@@ -0,0 +1,54 @@
+public class Dealer
+{
+	public static void main(String[] args) {
+		shuffleDeck();
+		do(){
+		    dealCards(){
+    		    give two random cards to Player, isViewable = true for both;
+    		    give two random cards to Dealer, isViewable = false for one and true for the other;
+    		    playerTotal = player.cacluateTotal();
+    		    dealerTotal = dealer.cacluateTotal();
+    		    while(!player.stands){
+    		        System.out.println("Would you like to Hit or stand?");
+    		        if(player.hits){
+        		        player.addCard();
+        		        playerTotal = player.cacluateTotal();
+    		        }
+    		        if(player.stands){
+    		            make isViewable true for the card that was originally false;
+    		            dealerTotal = dealer.cacluateTotal();
+    		            //don't know how to implement strategy on how
+    		            //to decide whether to add cards depending on score,
+    		            //but we'll figure that out later
+    		        }
+    		    }
+    		    
+	    	}
+		}while(playerTotal < 21)
+		
+		determineWinner(){
+		    if(playerTotal>21){
+		        System.out.println("Sorry Player, you lost!");
+    		}
+    		else if (dealerTotal > 21){
+    		    System.out.println("Congrats Player, you won!");
+    		}
+    		else if (playerTotal == 21){
+    		    System.out.println("Congrats Player, you have Blackjack!");
+    		}
+    		else if (dealerTotal == 21){
+    		    System.out.println("Sorry Player, the dealer has Blackjack!");
+    		}
+    		else if (dealerTotal < 21 && playerTotal < 21){
+    		    if(playerTotal > dealerTotal){
+    		        System.out.println("Congrats Player, you won!");
+    		    }
+    		    else if (dealerTotal > playerTotal){
+    		        System.out.println("Congrats Player, you won!");
+    		    }
+    		}
+		
+		
+	    }
+}
+
