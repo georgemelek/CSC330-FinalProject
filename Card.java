df2a24ac7152ee49a3523ecea52ea5d635e5075e
georgemelek <55765541+georgemelek@users.noreply.github.com>
2021-04-26 15:30:44 -0400

    added Gabe's file

---
+++ Card.java
@@ -0,0 +1,23 @@
+package final330;
+
+public class Card {
+
+	private Suit suit;
+	private Value value;
+	
+	public Card(Suit suit, Value value){
+		this.suit = suit;
+		this.value = value;
+	}
+	
+	public String toString(){
+		return this.value.toString() + "of" + this.suit.toString();
+	}
+	
+	public Value getValue(){
+		return this.value;
+	}
+
+
+	
+}
---
+++ Suit.java
@@ -0,0 +1,5 @@
+package final330;
+
+public enum Suit {
+	HEART, SPADE, DIAMOND, CLUB
+}
